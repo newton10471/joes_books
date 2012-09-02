ec7a838d6cd080ad731b12d90cbaa5fe33b41edc
Matt Newton <newton10471@gmail.com>
2012-09-02 09:02:15 -0400

    partial update of complete_round method

--- app/controllers/all_votes_controller.rb
+++ app/controllers/all_votes_controller.rb
@@ -8,6 +8,10 @@ class AllVotesController < ApplicationController
 
   def complete_round
     VotesController.second_round_started = true
+    # for bottom nth of voted-for books, set their vote count to 0
+    books = Book.all.select {|book| book.votes.count > 0}
+    books_to_keep = books.select {|book| book_votes_count > 0}
+    # set current vote count for each voted-on book to 1
   end
 
   def index
