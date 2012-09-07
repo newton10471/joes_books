b553d874c5f5f14f683dd70e4c1bb8e3f10c8a08
Matt Newton <newton10471@gmail.com>
2012-09-07 00:22:48 -0400

    CanCan integration and admin role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905005621) do
+ActiveRecord::Schema.define(:version => 20120907022738) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -33,10 +33,10 @@ ActiveRecord::Schema.define(:version => 20120905005621) do
   create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20120905005621) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
