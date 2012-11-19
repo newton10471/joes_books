d458034864854988f19e4ecd25cffdd1f9b0a482
Matt Newton <newton10471@gmail.com>
2012-11-18 21:21:57 -0500

    updates configuration to work with heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
+#APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
 
 # Initialize the rails application
 JoesBooks::Application.initialize!
