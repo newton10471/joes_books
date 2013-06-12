a9602381efd2122a671112a0bf0ccbff173a8fa9
Ken Lu <ken@seeice.com>
2013-06-12 09:17:08 -0400

    annoate for test and fixtures

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  author      :string(255)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#  votes_count :integer          default(0), not null
+#  asin        :string(255)
+#
+
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
