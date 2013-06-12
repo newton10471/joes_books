c0b748f4ab0694bcb4b9b195b6ab062623550d8a
Ken Lu <ken@seeice.com>
2013-06-12 09:16:51 -0400

    annotate for models

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: votes
+#
+#  id         :integer          not null, primary key
+#  book_id    :integer
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Vote < ActiveRecord::Base
   attr_accessible :book_id, :user_id
 
