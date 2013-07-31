96a653029ce1d8297af32c37a9ef1d72ac2f576a
Matt Newton <newton10471@gmail.com>
2013-07-30 22:00:51 -0400

    updates comments integration tests

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,17 +1,42 @@
 require 'spec_helper'
 
 describe "Comments" do
+  def sign_in_user
+    visit '/accounts/sign_in' 
+    user = FactoryGirl.create(:user)
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Sign in'
+  end
+
+
+  describe 'should be able to log in' do
+    it "login" do
+      visit '/accounts/sign_in' 
+      user = FactoryGirl.create(:user)
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_on 'Sign in'
+      expect(page).to have_content 'My Book List'
+    end
+
+    #it { should have_link 'Logout' }
+    #it { should have_content 'My Book List' }
+  end
+  
   describe "GET /comments" do
+    let(:user) { FactoryGirl.create :user}
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      sign_in_user
       get comments_path
-      binding.pry
+      # binding.pry
       response.status.should be(200)
     end
   end
 
-  describe "user can create a comment" do
-    # go to a book page
-    # create a comment on that book
-  end
+  #describe "user can create a comment" do
+  #  # go to a book page
+  #  # create a comment on that book
+  #end
 end
