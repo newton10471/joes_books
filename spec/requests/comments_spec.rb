4e28d91fe2caca4f4af6f33f8115732676761f08
Matt Newton <newton10471@gmail.com>
2013-08-04 12:19:22 -0400

    all comments specs pass

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -11,13 +11,18 @@ describe "Comments" do
 
   subject { page }
 
-  let(:user) { FactoryGirl.create(:user) }
+  before(:all) { 
+  }
 
-  before { sign_in_user user }
+  after(:all) {
+    User.destroy_all
+  }
 
   describe 'should be able to log in' do
     it "login" do
-      visit '/accounts/sign_in' 
+      @user = FactoryGirl.create(:user) 
+      sign_in_user @user 
+      visit root_path
       expect(page).to have_content 'My Book List'
     end
 
@@ -25,24 +30,29 @@ describe "Comments" do
     #it { should have_content 'My Book List' }
   end
 
-  describe "GET /comments" do
-    let(:book) { FactoryGirl.create :book, user: user } 
-    let(:comment) { FactoryGirl.create :comment, user: user, book: book } 
+  describe "visit /comments" do
+#    let(:book) { FactoryGirl.create :book, user: user } 
+#    let(:comment) { FactoryGirl.create :comment, user: user, book: book } 
     it "works! (now write some real specs)" do
-      get comments_path
-      # response.status.should be(200)
-      binding.pry
+      @user = FactoryGirl.create(:user) 
+      sign_in_user @user 
+      # get comments_path
+      visit comments_path
+      #response.status.should be(200)
       expect(page).to have_content 'Comment List'
     end
   end
 
   describe "user can create a book" do
     it 'should allow a user to create a book' do
+      @user = FactoryGirl.create(:user) 
+      sign_in_user @user 
       visit root_path
       click_on 'New Book'
       fill_in 'Title', with: 'Moby Dick'
       fill_in 'Author', with: 'Herman Melville'
-      fill_in 'Asin', with: '0143105957'
+      fill_in 'book_asin', with: '0143105957'
+      click_on 'Create Book'
       expect(page).to have_content 'Book was successfully created'
     end
   end
