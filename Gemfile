27cec83b13515c87682a7ee7be3a6ad04ce976a1
Pair <pair@basic.(none)>
2013-08-31 02:03:02 +0000

    remove ruby 2 form gem and typo

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+#ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 gem 'bootstrap-sass', '2.0.4'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-admin = User.create(email: 'admin@admin.com', password: 'password', password_confirmation: 'password', first_name: 'Amdin', last_name: 'Admin')
+admin = User.create(email: 'admin@admin.com', password: 'password', password_confirmation: 'password', first_name: 'Admin', last_name: 'Admin')
 admin.save(validate: false)
 admin.books.create(title: 'Joyland', author: 'Stephen King', asin: '1781162646')
 admin.books.create(title: 'The Long Walk', author: 'Stephen King', asin: '0451196716')
